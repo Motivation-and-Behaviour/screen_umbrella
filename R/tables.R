56858a3c4626c1a3570971918d39f25986b50f6d
Taren Sanders <taren.sanders@acu.edu.au>
2022-03-11 14:04:00 +1100

    Run styler on tables

--- R/tables.R
+++ R/tables.R
@@ -2,19 +2,23 @@
 
 # ------------------- TARGETS ------------------------
 
-make_tables <- 
+make_tables <-
   list(
     tar_target(
       tables_df,
-      make_table_data(rob_raw, effects_clean, reviews_raw)),
+      make_table_data(rob_raw, effects_clean, reviews_raw)
+    ),
     tar_target(
       reviews_tables,
-      make_desc_tables(tables_df)),
+      make_desc_tables(tables_df)
+    ),
     tar_target(
       export_tables,
       save_tables(reviews_tables),
       format = "file",
-      pattern = map(reviews_tables)))
+      pattern = map(reviews_tables)
+    )
+  )
 
 
 # ------------------- FUNCTIONS ----------------------
@@ -43,45 +47,49 @@ make_table_data <- function(rob_df, effects_df, reviews_df) {
       demographics_restrictions = demographics_consensus,
       earliest_study_year = earliest_study_publication_year_consensus,
       latest_study_year = latest_study_publication_year_consensus,
-      sample_age_mean_low = as.numeric(sapply(sample_age_lowest_study_mean_consensus, 
-                                   toString)),
-      sample_age_mean_high = as.numeric(sapply(sample_age_highest_study_mean_concensus, 
-                                    toString)) 
+      sample_age_mean_low = as.numeric(sapply(
+        sample_age_lowest_study_mean_consensus,
+        toString
+      )),
+      sample_age_mean_high = as.numeric(sapply(
+        sample_age_highest_study_mean_concensus,
+        toString
+      ))
     )
 
-  use_effects_ids <- 
-    effects %>% 
-    filter(use_effect) %>% 
-    distinct(covidence_id) %>% 
+  use_effects_ids <-
+    effects %>%
+    filter(use_effect) %>%
+    distinct(covidence_id) %>%
     pull(covidence_id)
-  
+
   # Join datasets
-  joined_df <- 
+  joined_df <-
     reviews %>%
     # Merge the datasets
     left_join(rob) %>%
-    left_join(effects) %>% 
-    mutate(table = if_else(covidence_id %in% use_effects_ids, "main","supp"))
-  
+    left_join(effects) %>%
+    mutate(table = if_else(covidence_id %in% use_effects_ids, "main", "supp"))
+
   return(joined_df)
 }
-  
+
 make_desc_tables <- function(tables_df) {
-  
   bullet_list <- function(column) {
     paste0("<ul style='padding-left:10px'><li>",
-           paste(column %>% unique() %>% sort(),
-                 collapse = "</li><li>"
-           ), "</li></ul>",
-           collapse = ""
+      paste(column %>% unique() %>% sort(),
+        collapse = "</li><li>"
+      ), "</li></ul>",
+      collapse = ""
     )
   }
-  
+
   unique_list <- function(column) {
     paste(column %>% unique() %>% sort(),
-          collapse = "<br><br>")
+      collapse = "<br><br>"
+    )
   }
-  
+
   rob_cols <- c("#b7e1cd", "#fce8b2", "#f4c7c3")
 
   # Main table
@@ -93,10 +101,12 @@ make_desc_tables <- function(tables_df) {
       exposures_assessed = map(bullet_list(plain_language_exposure), gt::html),
     ) %>%
     ungroup() %>%
-    select(-plain_language_outcome,
-           -plain_language_exposure,
-           -use_effect,
-           -table) %>%
+    select(
+      -plain_language_outcome,
+      -plain_language_exposure,
+      -use_effect,
+      -table
+    ) %>%
     distinct() %>%
     mutate(
       sample_ages = if_else(
@@ -122,36 +132,49 @@ make_desc_tables <- function(tables_df) {
         "eligibility_criteria_predefined_and_specified":"heterogeneity_assessed"
       ),
       str_to_title
-    )  %>%
+    ) %>%
     # Reorder the columns
-    select(-starts_with("sample_age_mean"),
-           -demographics_restrictions,
-           -covidence_id) %>%
+    select(
+      -starts_with("sample_age_mean"),
+      -demographics_restrictions,
+      -covidence_id
+    ) %>%
     relocate(any_of(c(
       "sample_ages", "outcomes_assessed", "exposures_assessed"
     )),
-    .after = latest_study_year) %>% 
+    .after = latest_study_year
+    ) %>%
     arrange(first_author, year)
-  
-  gt_table_main <- 
-    base_table %>% 
-    gt(caption = "Review characteristics and quality assessment for studies providing unique effects") %>% 
+
+  gt_table_main <-
+    base_table %>%
+    gt(caption = "Review characteristics and quality assessment for studies providing unique effects") %>%
     # Add color to ROB
     data_color(
-      columns = 
+      columns =
         eligibility_criteria_predefined_and_specified:heterogeneity_assessed,
-      colors = scales::col_factor(palette = rob_cols,
-                                  levels = c("Low", "Unclear", "High"))) %>% 
-    cols_align(columns = 
-                 eligibility_criteria_predefined_and_specified:heterogeneity_assessed,
-               align = "center") %>% 
-    tab_header(html("<strong>Study Characteristics</strong>"), 
-               subtitle = "Review characteristics and quality assessment for studies providing unique effects") %>% 
-    tab_spanner(label = "Review Characteristics",
-                columns = first_author:exposures_assessed) %>% 
-    tab_spanner(label = "Quality Assessment",
-                columns = eligibility_criteria_predefined_and_specified:heterogeneity_assessed) %>%
-    cols_merge_range(col_begin = earliest_study_year, col_end = latest_study_year) %>% 
+      colors = scales::col_factor(
+        palette = rob_cols,
+        levels = c("Low", "Unclear", "High")
+      )
+    ) %>%
+    cols_align(
+      columns =
+        eligibility_criteria_predefined_and_specified:heterogeneity_assessed,
+      align = "center"
+    ) %>%
+    tab_header(html("<strong>Study Characteristics</strong>"),
+      subtitle = "Review characteristics and quality assessment for studies providing unique effects"
+    ) %>%
+    tab_spanner(
+      label = "Review Characteristics",
+      columns = first_author:exposures_assessed
+    ) %>%
+    tab_spanner(
+      label = "Quality Assessment",
+      columns = eligibility_criteria_predefined_and_specified:heterogeneity_assessed
+    ) %>%
+    cols_merge_range(col_begin = earliest_study_year, col_end = latest_study_year) %>%
     cols_label(
       first_author = "First Author",
       year = "Year",
@@ -168,30 +191,49 @@ make_desc_tables <- function(tables_df) {
       included_studies_listed_with_important_characteristics_and_results_of_each = "Studies Listed",
       publication_bias_assessed = "Pub. Bias Assessed",
       heterogeneity_assessed = "Heterog. Assessed"
-    ) %>% 
-    cols_align(columns = 
-                 first_author:exposures_assessed,
-               align = "left") %>% 
-
-    tab_footnote(footnote = "Where provided",
-                 locations = cells_column_labels(sample_ages)) %>% 
+    ) %>%
+    cols_align(
+      columns =
+        first_author:exposures_assessed,
+      align = "left"
+    ) %>%
+    tab_footnote(
+      footnote = "Where provided",
+      locations = cells_column_labels(sample_ages)
+    ) %>%
     # ROB footnotes
-    tab_footnote(footnote = "Eligibility criteria predefined and specified",
-                 locations = cells_column_labels(eligibility_criteria_predefined_and_specified)) %>% 
-    tab_footnote(footnote = "Literature search strategy comprehensive and systematic",
-                 locations = cells_column_labels(literature_search_strategy_comprehensive_and_systematic)) %>% 
-    tab_footnote(footnote = "Dual independent screening & review",
-                 locations = cells_column_labels(dual_independent_screening_review)) %>% 
-    tab_footnote(footnote = "Dual independent quality assessment",
-                 locations = cells_column_labels(dual_independent_quality_assessment)) %>% 
-    tab_footnote(footnote = "Included studies listed with important characteristics and results of each",
-                 locations = cells_column_labels(included_studies_listed_with_important_characteristics_and_results_of_each)) %>% 
-    tab_footnote(footnote = "Publication bias assessed",
-                 locations = cells_column_labels(publication_bias_assessed)) %>% 
-    tab_footnote(footnote = "Heterogeneity assessed",
-                 locations = cells_column_labels(heterogeneity_assessed)) %>% 
-    tab_footnote(footnote = "Items are from the National Health, Lung and Blood Institute’s Quality Assessment of Systematic Reviews and Meta-Analyses tool. Note that we excluded the first item of the tool.",
-                 locations = cells_column_spanners("Quality Assessment")) %>% 
+    tab_footnote(
+      footnote = "Eligibility criteria predefined and specified",
+      locations = cells_column_labels(eligibility_criteria_predefined_and_specified)
+    ) %>%
+    tab_footnote(
+      footnote = "Literature search strategy comprehensive and systematic",
+      locations = cells_column_labels(literature_search_strategy_comprehensive_and_systematic)
+    ) %>%
+    tab_footnote(
+      footnote = "Dual independent screening & review",
+      locations = cells_column_labels(dual_independent_screening_review)
+    ) %>%
+    tab_footnote(
+      footnote = "Dual independent quality assessment",
+      locations = cells_column_labels(dual_independent_quality_assessment)
+    ) %>%
+    tab_footnote(
+      footnote = "Included studies listed with important characteristics and results of each",
+      locations = cells_column_labels(included_studies_listed_with_important_characteristics_and_results_of_each)
+    ) %>%
+    tab_footnote(
+      footnote = "Publication bias assessed",
+      locations = cells_column_labels(publication_bias_assessed)
+    ) %>%
+    tab_footnote(
+      footnote = "Heterogeneity assessed",
+      locations = cells_column_labels(heterogeneity_assessed)
+    ) %>%
+    tab_footnote(
+      footnote = "Items are from the National Health, Lung and Blood Institute’s Quality Assessment of Systematic Reviews and Meta-Analyses tool. Note that we excluded the first item of the tool.",
+      locations = cells_column_spanners("Quality Assessment")
+    ) %>%
     # Styling
     cols_width(
       first_author ~ pct(6),
@@ -204,48 +246,52 @@ make_desc_tables <- function(tables_df) {
       exposures_assessed ~ pct(17),
       c(eligibility_criteria_predefined_and_specified:heterogeneity_assessed) ~ pct(3)
     ) %>%
-    tab_options(heading.align = "left",
-                column_labels.border.top.color = "black",
-                column_labels.border.top.width = px(3),
-                column_labels.border.bottom.color = "black",
-                table_body.hlines.color = "white",
-                table.border.bottom.color = "black",
-                table.border.top.color = "black",
-                table.border.bottom.width = px(3),
-               table.border.top.width = px(3),
-               footnotes.padding = px(0),
-               column_labels.padding = px(3),
-               data_row.padding = px(1),
-               table.font.size = pct(80)
-               ) %>% 
-    tab_style(style = list(cell_text(weight = "bold",align = "left")),
+    tab_options(
+      heading.align = "left",
+      column_labels.border.top.color = "black",
+      column_labels.border.top.width = px(3),
+      column_labels.border.bottom.color = "black",
+      table_body.hlines.color = "white",
+      table.border.bottom.color = "black",
+      table.border.top.color = "black",
+      table.border.bottom.width = px(3),
+      table.border.top.width = px(3),
+      footnotes.padding = px(0),
+      column_labels.padding = px(3),
+      data_row.padding = px(1),
+      table.font.size = pct(80)
+    ) %>%
+    tab_style(
+      style = list(cell_text(weight = "bold", align = "left")),
       locations = cells_column_labels(everything())
-    ) %>% 
-    tab_style(style = list(cell_text(weight = "bold")),
+    ) %>%
+    tab_style(
+      style = list(cell_text(weight = "bold")),
       locations = cells_column_spanners(everything())
-    ) 
-  
-  reviews_tables <- list(length=1)
-  
+    )
+
+  reviews_tables <- list(length = 1)
+
   reviews_tables[[1]]$table <- gt_table_main
   reviews_tables[[1]]$filename <- "Descriptive table.pdf"
-  
+
   return(reviews_tables)
 }
 
 
 save_tables <- function(tables) {
   file_name <- here::here("figure", tables[[1]]$filename)
-  
+
   gtsave(
     data = tables[[1]]$table,
     filename = tables[[1]]$filename,
     path = here::here("figure")
   )
-  
+
   # Upload to GDrive
-  drive_put(file_name, 
-            path = as_id("https://drive.google.com/drive/folders/1xvn1B4bGH7hr6yBvDGUfEF7F_eO3Qeml"))
-  
+  drive_put(file_name,
+    path = as_id("https://drive.google.com/drive/folders/1xvn1B4bGH7hr6yBvDGUfEF7F_eO3Qeml")
+  )
+
   return(file_name)
 }
