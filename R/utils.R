661194a39f7783fec20a6fb63d55fe5fb9804a0c
Taren Sanders <taren.sanders@gmail.com>
2023-02-21 21:35:12 +1100

    Small tidy up

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,7 @@
 read_sheet <- function(file) {
-  readr::read_csv(file = file, na = c("-999", "", "#N/A")) %>%
+  readr::read_csv(
+    file = file, na = c("-999", "", "#N/A"), show_col_types = FALSE
+  ) %>%
     janitor::clean_names()
 }
 
