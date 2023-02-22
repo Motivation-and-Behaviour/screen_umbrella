aeb2630c3ac7620bb8bd2b42f4d8f2ee46e7e8dd
Taren Sanders <taren.sanders@acu.edu.au>
2023-02-22 12:32:26 +1100

    Suppress incorrect warnings in convert_effects

--- R/convert_effects.R
+++ R/convert_effects.R
@@ -10,50 +10,59 @@
 convert_effects <- function(data) {
   data_converted <- data %>%
     mutate(
-      converted_ci_lower_bound = if_else(
+      cilb = if_else(
         is.na(value_ci_lower_bound) & !is.na(value_raw_se),
         value - 1.96 * value_raw_se,
         value_ci_lower_bound
       ),
-      converted_ci_upper_bound = if_else(
+      ciub = if_else(
         is.na(value_ci_lower_bound) & !is.na(value_raw_se),
         value - 1.96 * value_raw_se,
         value_ci_lower_bound,
       ),
-      converted_value = case_when(
+      r = case_when(
         is.na(value) ~ NA_real_,
         stat_test_clean == "r" ~ value,
         stat_test_clean == "b" ~ b2r(value),
         stat_test_clean == "d" ~ effectsize::d_to_r(value),
-        stat_test_clean == "or" ~ effectsize::oddsratio_to_r(value),
+        stat_test_clean == "or" ~
+          suppressWarnings(effectsize::oddsratio_to_r(value)),
         stat_test_clean == "z" ~ correlation::z_fisher(z = value)
       ),
-      converted_ci_lower_bound = case_when(
-        is.na(converted_ci_lower_bound) ~ NA_real_,
-        stat_test_clean == "r" ~ converted_ci_lower_bound,
-        stat_test_clean == "b" ~ b2r(converted_ci_lower_bound),
-        stat_test_clean == "d" ~ effectsize::d_to_r(converted_ci_lower_bound),
+      cilb = case_when(
+        is.na(cilb) ~ NA_real_,
+        stat_test_clean == "r" ~ cilb,
+        stat_test_clean == "b" ~ b2r(cilb),
+        stat_test_clean == "d" ~ effectsize::d_to_r(cilb),
         stat_test_clean == "or" ~
-          effectsize::oddsratio_to_r(converted_ci_lower_bound),
-        stat_test_clean == "z" ~
-          correlation::z_fisher(z = converted_ci_lower_bound)
+          suppressWarnings(effectsize::oddsratio_to_r(cilb)),
+        stat_test_clean == "z" ~ correlation::z_fisher(z = cilb)
       ),
-      converted_ci_upper_bound = case_when(
-        is.na(converted_ci_upper_bound) ~ NA_real_,
-        stat_test_clean == "r" ~ converted_ci_upper_bound,
-        stat_test_clean == "b" ~ b2r(converted_ci_upper_bound),
-        stat_test_clean == "d" ~ effectsize::d_to_r(converted_ci_upper_bound),
+      ciub = case_when(
+        is.na(ciub) ~ NA_real_,
+        stat_test_clean == "r" ~ ciub,
+        stat_test_clean == "b" ~ b2r(ciub),
+        stat_test_clean == "d" ~ effectsize::d_to_r(ciub),
         stat_test_clean == "or" ~
-          effectsize::oddsratio_to_r(converted_ci_upper_bound),
-        stat_test_clean == "z" ~
-          correlation::z_fisher(z = converted_ci_upper_bound)
+          suppressWarnings(effectsize::oddsratio_to_r(ciub)),
+        stat_test_clean == "z" ~ correlation::z_fisher(z = ciub)
       ),
-      converted_value_z = correlation::z_fisher(r = converted_value),
-      converted_ci_lower_bound_z =
-        correlation::z_fisher(r = converted_ci_lower_bound),
-      converted_ci_upper_bound_z =
-        correlation::z_fisher(r = converted_ci_upper_bound)
+      z = correlation::z_fisher(r = r),
+      cilb_z = suppressWarnings(correlation::z_fisher(r = cilb)),
+      ciub_z = suppressWarnings(correlation::z_fisher(r = ciub))
     )
+
+  # The suppressed warnings are related to NaNs. Check that there were no new
+  # NaNs introduced by the conversion
+  stopifnot(
+    length(is.na(data_converted$value)) == length(is.na(data_converted$r)),
+    length(is.na(data_converted$value_ci_lower_bound)) <=
+      length(is.na(data_converted$cilb)),
+    length(is.na(data_converted$value_ci_upper_bound)) <=
+      length(is.na(data_converted$ciub)),
+    length(is.na(data_converted$cilb)) == length(is.na(data_converted$cilb_z)),
+    length(is.na(data_converted$ciub)) == length(is.na(data_converted$ciub_z))
+  )
 
   return(data_converted)
 }
