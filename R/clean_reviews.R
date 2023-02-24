f82b2429090ef3235f6f69bfc3777199c126b636
Taren Sanders <taren.sanders@acu.edu.au>
2023-02-24 15:56:02 +1100

    Move codes to be available elsewhere

--- R/clean_reviews.R
+++ R/clean_reviews.R
@@ -3,97 +3,13 @@
 #' .. content for \details{} ..
 #'
 #' @title
-#' @param reviews
+#' @param reviews_raw
 #' @return
 #' @author Taren Sanders
 #' @export
-clean_reviews <- function(reviews) {
-  mixed_codes <- c(
-    "<=18 years",
-    "<18 years",
-    "<20 years",
-    "0-21 years old",
-    "1 - 18 years",
-    "1-18 years",
-    "10 - 19 years",
-    "10-19 years",
-    "10-20 years old",
-    "10-24 years",
-    "11-20",
-    "2-19 years",
-    "3-18 years",
-    "5-18 years",
-    "5-19.9",
-    "6-18 years",
-    "6-18 years (or grade 1-12)",
-    "6-19",
-    "6-21 years",
-    "All",
-    "Children; Adolescents",
-    "Children/adolescents",
-    "Include: children and adolescents",
-    "K-12",
-    "None specified",
-    "PreK-12th grade",
-    "Preschool, K-12, undergraduate (graduate), and mixed-stage learners",
-    "Primary or secondary school",
-    "School-age Children",
-    "Under 18"
-  )
-  adolescents_codes <- c(
-    "11-18 years",
-    "12-17 years",
-    "12-30 years",
-    "13-18",
-    "15-30 years old",
-    "adolescence",
-    "Adolescents",
-    "at least 13 years",
-    "School-age Children (Middle School)",
-    "School-age Children (Middle/High School)",
-    "school-age_high school",
-    "School-age_High School"
-  )
-  children_codes <- c(
-    "12 or under",
-    "12 or younger",
-    "3-12 years",
-    "6-11 years",
-    "7-12",
-    "8-14 years",
-    "Average 6 years, all must be less than 8 years",
-    "childhood",
-    "children",
-    "Early childhood; School-age Children (Primary/Elementary)",
-    "Early childhood/pre-school; School-age Children (Early Primary/Elementary)",
-    "Elementary school students",
-    "grades 4-7",
-    "K - Grade 8",
-    "Kindergarten-Grade 6",
-    "older than 8",
-    "Pre K to Grade 3",
-    "Preschool to Grade 3 (3-9 years old)",
-    "School-age Children (Early Primary/Elementary)",
-    "School-age Children (Primary/Elementary)",
-    "School-age Children (Primary/Elementary/Middle School)"
-  )
-  young_children_codes <- c(
-    "0-5",
-    "0-6",
-    "1 month - 4.99 years old",
-    "1-6 years & prior to school entry",
-    "1-8 years old",
-    "Early childhood/pre-school",
-    "Infants",
-    "Infants, toddlers, and preschoolers (0-7 years)",
-    "pre-school",
-    "Preschoolers",
-    "Toddlers",
-    "younger than 8"
-  )
-
+clean_reviews <- function(reviews_raw) {
   reviews_clean <-
-    reviews %>%
+    reviews_raw %>%
     mutate(
       demographics_coded = case_when(
         demographics %in% mixed_codes ~ "Mixed",
@@ -127,5 +43,9 @@ clean_reviews <- function(reviews) {
           "Children",
         TRUE ~ demographics_coded
       )
+    ) %>%
+    # Add some other helpful columns
+    mutate(
+      author_year = paste(first_author, ", ", year_of_publication, sep = "")
     )
 }
